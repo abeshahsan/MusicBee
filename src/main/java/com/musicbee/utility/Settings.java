332c9c9cd705aac737daeca48a54417985c29bc4
abeshahsan <abeshahsan2002@gmail.com>
2023-06-03 00:57:25 +0600

    cleared (nothing necessary was there)

--- src/main/java/com/musicbee/utility/Settings.java
+++ src/main/java/com/musicbee/utility/Settings.java
@@ -1,27 +1,5 @@
 package com.musicbee.utility;
 
-import javafx.stage.Stage;
-
 public class Settings {
-    private static final double minWidth = 1050;
-    private static final double minHeight = 800;
-    private static double width = 950;
-    private static double height = 800;
 
-    public static double getMinWidth() {
-        return minWidth;
-    }
-    public static double getMinHeight() {
-        return minHeight;
-    }
-    public static void setWidth(double width) {
-        Settings.width = width;
-    }
-    public static void setMinHeight(double height) {
-        Settings.height = height;
-    }
-    public static void updateWidthHeight(Stage stage) {
-        Settings.height = stage.getHeight();
-        Settings.width = stage.getWidth();
-    }
 }
